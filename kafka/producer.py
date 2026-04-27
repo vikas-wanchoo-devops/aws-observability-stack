c21c89db97a769c093f0872bce483ab0606e2301
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 17:08:54 +0530

    Integrate AWS CloudWatch logs with Kafka producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -1,4 +1,15 @@
+import boto3, json
 from kafka import KafkaProducer
+
+logs = boto3.client('logs', region_name='eu-north-1')
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
-producer.send('flask-logs', b'Test log message')
+
+response = logs.filter_log_events(
+    logGroupName='/ecs/assaabloy-app-task',
+    limit=10
+)
+
+for event in response['events']:
+    producer.send('flask-logs', json.dumps(event).encode('utf-8'))
+
 producer.flush()
