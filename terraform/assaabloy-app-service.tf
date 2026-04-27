2594c85014ee6bdadcf259fabe9019a277d352d4
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 21:44:24 +0530

    Create assaabloy-app-service.tf

---
+++ terraform/assaabloy-app-service.tf
@@ -0,0 +1,29 @@
+# terraform/assaabloy-app-service.tf
+# ECS Service definition for Assa Abloy Application
+
+resource "aws_ecs_service" "app" {
+  name            = "assaabloy-app-service"
+  cluster         = aws_ecs_cluster.app_cluster.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.private1.id, aws_subnet.private2.id]
+    security_groups = [aws_security_group.app_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "assaabloy-app"
+    container_port   = 8080
+  }
+
+  deployment_controller {
+    type = "ECS"
+  }
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+}
