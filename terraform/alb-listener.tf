bb9c76a8764427ef13bfccf2b357b0cd71237359
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 20:55:59 +0530

    Update alb-listener.tf

--- terraform/alb-listener.tf
+++ terraform/alb-listener.tf
@@ -1,5 +1,17 @@
-# ALB listener for HTTP traffic on port 80
+variable "create_listener" {
+  type    = bool
+  default = false
+}
+
+# Try to read existing listener
+data "aws_lb_listener" "app" {
+  count = var.create_listener ? 0 : 1
+  arn   = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:listener/app/assaabloy-app-alb/6f2878a5c4470aa5/1bba9a3cfb100633"
+}
+
+# Create listener only if flag is true
 resource "aws_lb_listener" "app" {
+  count             = var.create_listener ? 1 : 0
   load_balancer_arn = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:loadbalancer/app/assaabloy-app-alb/6f2878a5c4470aa5"
   port              = 80
   protocol          = "HTTP"
