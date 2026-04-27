0d3b9158f0a9aee05803d9f8503637761d239d92
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 17:41:12 +0530

    Add AWS resources for Prometheus service

--- terraform/prometheus-service.tf
+++ terraform/prometheus-service.tf
@@ -1,3 +1,33 @@
+resource "aws_lb_target_group" "prometheus" {
+  name     = "prometheus-tg"
+  port     = 9090
+  protocol = "HTTP"
+  vpc_id   = "vpc-0b5d7248bdde16ef7"
+  target_type = "ip"
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "prometheus" {
+  listener_arn = aws_lb_listener.app.arn
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.prometheus.arn
+  }
+  condition {
+    host_header {
+      values = ["prometheus.assaabloy-app-alb-373654538.eu-north-1.elb.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_ecs_task_definition" "prometheus" {
   family                   = "prometheus-task"
   network_mode             = "awsvpc"
@@ -29,4 +59,12 @@ resource "aws_ecs_service" "prometheus" {
     security_groups = ["sg-04df50a141a12d19a"]
     assign_public_ip = true
   }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.prometheus.arn
+    container_name   = "prometheus"
+    container_port   = 9090
+  }
+
+  depends_on = [aws_lb_listener_rule.prometheus]
 }
