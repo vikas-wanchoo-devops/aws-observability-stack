5a3c35a8bc741f12da78bd2110bf5e070e2c2068
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 20:45:44 +0530

    Update grafana-service.tf

--- terraform/grafana-service.tf
+++ terraform/grafana-service.tf
@@ -1,42 +1,26 @@
-resource "aws_ecs_task_definition" "grafana" {
-  family                   = "grafana-task"
+# Reference existing target group
+data "aws_lb_target_group" "prometheus" {
+  arn = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:targetgroup/prometheus-tg-ecs/43148ae3c7075f70"
+}
+
+# Manage task definition
+resource "aws_ecs_task_definition" "prometheus" {
+  family                   = "prometheus-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
 
   container_definitions = jsonencode([{
-    name      = "grafana"
-    image     = "grafana/grafana:latest"
+    name      = "prometheus"
+    image     = "prom/prometheus:latest"
     essential = true
-    portMappings = [{ containerPort = 3000, hostPort = 3000 }]
-    environment = [
-      { name = "GF_SECURITY_ADMIN_USER", value = "admin" },
-      { name = "GF_SECURITY_ADMIN_PASSWORD", value = "admin" }
-    ]
+    portMappings = [{ containerPort = 9090, hostPort = 9090 }]
   }])
 }
 
-resource "aws_ecs_service" "grafana" {
-  name            = "grafana-service"
-  cluster         = "assaabloy-app-cluster"
-  task_definition = aws_ecs_task_definition.grafana.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets = [
-      "subnet-0d16d36a33d1c1f22",
-      "subnet-013e51f5fbc1318cb",
-      "subnet-0a4e24f116d3364f9"
-    ]
-    security_groups = ["sg-04df50a141a12d19a"]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:targetgroup/grafana-tg-ecs/78413275af16e77b"
-    container_name   = "grafana"
-    container_port   = 3000
-  }
+# Reference existing ECS service
+data "aws_ecs_service" "prometheus" {
+  cluster      = "assaabloy-app-cluster"
+  service_name = "prometheus-service"
 }
