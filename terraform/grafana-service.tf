b3e31f5f62f37e710d7d4dfee371e051f691e6ba
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 20:46:09 +0530

    Update grafana-service.tf

--- terraform/grafana-service.tf
+++ terraform/grafana-service.tf
@@ -1,26 +1,30 @@
 # Reference existing target group
-data "aws_lb_target_group" "prometheus" {
-  arn = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:targetgroup/prometheus-tg-ecs/43148ae3c7075f70"
+data "aws_lb_target_group" "grafana" {
+  arn = "arn:aws:elasticloadbalancing:eu-north-1:879696522469:targetgroup/grafana-tg-ecs/78413275af16e77b"
 }
 
 # Manage task definition
-resource "aws_ecs_task_definition" "prometheus" {
-  family                   = "prometheus-task"
+resource "aws_ecs_task_definition" "grafana" {
+  family                   = "grafana-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
 
   container_definitions = jsonencode([{
-    name      = "prometheus"
-    image     = "prom/prometheus:latest"
+    name      = "grafana"
+    image     = "grafana/grafana:latest"
     essential = true
-    portMappings = [{ containerPort = 9090, hostPort = 9090 }]
+    portMappings = [{ containerPort = 3000, hostPort = 3000 }]
+    environment = [
+      { name = "GF_SECURITY_ADMIN_USER", value = "admin" },
+      { name = "GF_SECURITY_ADMIN_PASSWORD", value = "admin" }
+    ]
   }])
 }
 
 # Reference existing ECS service
-data "aws_ecs_service" "prometheus" {
+data "aws_ecs_service" "grafana" {
   cluster      = "assaabloy-app-cluster"
-  service_name = "prometheus-service"
+  service_name = "grafana-service"
 }
