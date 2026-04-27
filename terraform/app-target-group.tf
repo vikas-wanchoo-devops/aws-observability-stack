55dc09f9bb8979e49615895e7138c70453a97784
Vikas Kumar Wanchoo <wanchoo@live.in>
2026-04-27 21:55:07 +0530

    Add AWS LB target group configuration

---
+++ terraform/app-target-group.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_target_group" "app_tg" {
+  name     = "assaabloy-app-tg"
+  port     = 5000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+}
